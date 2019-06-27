9d79f93b85e61e6df7129d39c0f2232a90c66d65
Denis Todorov <denis.todorov@addpeople.co.uk>
2019-06-27 22:25:10 +0100

    added few default params for env and init script

--- acme/initialise.sh
+++ acme/initialise.sh
@@ -4,8 +4,10 @@ if [ ! -f ".env" ]; then
 
     composer install
     composer dump-autoload -o
+    cp .env.example .env
+    php artisan key:generate
     touch database/database.sqlite
-    php artisan migrate --seed
+    php artisan migrate --seed -n
 
 fi
 
